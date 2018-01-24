5b9a14a4af5d7d174fed95f03670c507f7b57bfb
Don Alvarez <don@acceleratedpictures.com>
2018-01-23 22:48:31 -0800

    Converting electron main from js to ts

--- _main/index.js
+++ _main/index.js
@@ -1,49 +1,42 @@
-'use strict';
-const electron = require('electron');
-
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-const crashReporter = electron.crashReporter;
-
-const url = require('url');
-const nodeEnv = process.env.NODE_ENV;
-
-//crashReporter.start();
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+"use strict";
+exports.__esModule = true;
+var electron_1 = require("electron");
+var path = require("path");
+var url = require("url");
+var nodeEnv = process.env.NODE_ENV;
+// crashReporter.start();
+electron_1.app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        electron_1.app.quit();
+    }
 });
-
-app.on('ready', () => {
-  if (nodeEnv === 'development') {
-    const sourceMapSupport = require('source-map-support');
-    sourceMapSupport.install();
-  }
-  createWindow();
-})
-
+electron_1.app.on('ready', function () {
+    if (nodeEnv === 'development') {
+        var sourceMapSupport = require('source-map-support');
+        sourceMapSupport.install();
+    }
+    createWindow();
+});
 function createWindow() {
-  // { width: 1024, height: 728 }
-  const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize;
-  const win = new BrowserWindow({width, height});
-  
-  if (nodeEnv === 'development') {
-    //delay 1000ms to wait for webpack-dev-server start
-    setTimeout(function(){
-      win.loadURL(url.format({
-        pathname: "localhost:3000/electron.html",
-        protocol: 'http:',
-        slashes: true
-      }));
-      win.webContents.openDevTools();
-    },1000);
-  } else {
-    win.loadURL(url.format({
-      pathname: path.join(path.resolve(__dirname, './dist'), 'electron.html'),
-      protocol: 'file:',
-      slashes: true
-    }));
-  }
+    // { width: 1024, height: 728 }
+    var _a = electron_1.screen.getPrimaryDisplay().workAreaSize, width = _a.width, height = _a.height;
+    var win = new electron_1.BrowserWindow({ width: width, height: height });
+    if (nodeEnv === 'development') {
+        //delay 1000ms to wait for webpack-dev-server start
+        setTimeout(function () {
+            win.loadURL(url.format({
+                pathname: "localhost:3000/electron.html",
+                protocol: 'http:',
+                slashes: true
+            }));
+            win.webContents.openDevTools();
+        }, 1000);
+    }
+    else {
+        win.loadURL(url.format({
+            pathname: path.join(path.resolve(__dirname, './dist'), 'electron.html'),
+            protocol: 'file:',
+            slashes: true
+        }));
+    }
 }
---
+++ _main/index.ts
@@ -0,0 +1,45 @@
+import { app, BrowserWindow, screen } from "electron";
+import * as path from "path";
+import * as url from "url";
+
+const nodeEnv = process.env.NODE_ENV;
+
+// crashReporter.start();
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', () => {
+  if (nodeEnv === 'development') {
+    const sourceMapSupport = require('source-map-support');
+    sourceMapSupport.install();
+  }
+  createWindow();
+})
+
+function createWindow() {
+  // { width: 1024, height: 728 }
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+  const win = new BrowserWindow({ width, height });
+  
+  if (nodeEnv === 'development') {
+    //delay 1000ms to wait for webpack-dev-server start
+    setTimeout(function(){
+      win.loadURL(url.format({
+        pathname: "localhost:3000/electron.html",
+        protocol: 'http:',
+        slashes: true
+      }));
+      win.webContents.openDevTools();
+    },1000);
+  } else {
+    win.loadURL(url.format({
+      pathname: path.join(path.resolve(__dirname, './dist'), 'electron.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
+  }
+}
