631715e6348d9606f7ba132b60ede35c9a02ade9
Don Alvarez <don@acceleratedpictures.com>
2018-02-03 13:51:06 -0800

    clean gh-pages branch

--- src/renderer/components/HybridAppPage/styles.scss.d.ts
+++
@@ -1,2 +0,0 @@
-export const backButton: string;
-export const webView: string;
--- src/shared/components/CounterPage/styles.scss.d.ts
+++
@@ -1,4 +0,0 @@
-export const backButton: string;
-export const counter: string;
-export const btnGroup: string;
-export const btn: string;
